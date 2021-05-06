97ef91654883946c5f31d00ce2f7c7a6dc2e9a3d
Fabio Braga de Oliveira <fabiool@amazon.com>
2021-05-06 17:46:47 +0200

    Add error handling

--- geocoding_lambda/lambda_function.py
+++ geocoding_lambda/lambda_function.py
@@ -6,19 +6,28 @@ import boto3
 
 
 def lambda_handler(event, context):
-  # load the side-loaded Amazon Location Service model; needed during Public Preview
-  os.environ["AWS_DATA_PATH"] = os.environ["LAMBDA_TASK_ROOT"]
+    # load the side-loaded Amazon Location Service model; needed during Public Preview
+    os.environ["AWS_DATA_PATH"] = os.environ["LAMBDA_TASK_ROOT"]
 
-  client = boto3.client("location")
-  arguments = event["arguments"]
+    client = boto3.client("location")
+    arguments = event["arguments"]
 
-  results = []
+    results = []
 
-  for arg in arguments:
-    # populate the response list in the same orders as the arguments list
+    try:
+        for arg in arguments:
+            # populate the response list in the same orders as the arguments list
 
-  return {
-    "success": true,
-    "num_records": len(response),
-    "results": results
-  }
+        return {
+            "success": true,
+            "num_records": len(response),
+            "results": results
+        }
+    except ClientError as e:
+        log.error('Error: {}'.format(e))
+        return {
+            "success": false,
+            "error_msg": e
+            "num_records": 0,
+            "results": []
+        }
