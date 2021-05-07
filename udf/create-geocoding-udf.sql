1d26a138bc209402748339e7fd6f43a1f6116cef
Fabio Braga de Oliveira <fabiool@amazon.com>
2021-05-07 16:14:26 +0200

    Add SQL to create geocoding function

--- udf/create-geocoding-udf.sql
+++ udf/create-geocoding-udf.sql
@@ -1,1 +1,11 @@
-# See documentation on https://docs.aws.amazon.com/redshift/latest/dg/udf-creating-a-lambda-sql-udf.html
+-- See documentation on https://docs.aws.amazon.com/redshift/latest/dg/udf-creating-a-lambda-sql-udf.html
+grant usage on language exfunc to PUBLIC;
+CREATE OR REPLACE EXTERNAL FUNCTION public.geocode_address(
+    VARCHAR, -- The address, name, city, or region to be used in the search. In free-form text format. For example, 123 Any Street. 
+    VARCHAR, -- Bias position, a JSON array of doubles. Searches for results closest to the given position. An optional parameter defined by longitude, and latitude.
+    VARCHAR, -- Filter countries, a JSON array of strings. Limits the search to the given a list of countries/regions. An optional parameter.  
+)
+RETURNS VARCHAR -- A JSON with the address components (Country, PostalCode, AddressNumber, Street, etc...)
+STABLE
+LAMBDA 'RedshiftUDFGeocodeFunction'
+IAM_ROLE 'arn:aws:iam::<AccountId>:role/Redshift-Lambda-role';
